dc65e3761335d5a3b2ebacc11b0bfa531a371bc1
Ryan <ryanrdk@icloud.com>
2017-11-13 22:16:21 +0200

    Created AppIcon, Serialised JSON data, Appended consumer key

--- Top500Px/ViewController.swift
+++ Top500Px/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct topPics {
+    let imageURL: String
+    let title: String
+    let photographer: String
+}
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let consumerKey = "q89i18uOtcklwE2S1XqMjXY9Li4shDrVQzLTGUVq"
+        let jsonUrlString = "https://api.500px.com/v1/photos?feature=popular&consumer_key=\(consumerKey)"
+        let url = URL(string: jsonUrlString)
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, err) in
+            
+            guard let data = data else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                print(json)
+            } catch let jsonErr {
+                print("Attempt at serializing JSON caused an error:", jsonErr)
+            }
+        }.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
